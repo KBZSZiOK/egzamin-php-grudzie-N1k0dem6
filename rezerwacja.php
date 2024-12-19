b979a16314922d03c1c80a3bc38a789137323e75
N1k0dem6 <150299369+N1k0dem6@users.noreply.github.com>
2024-12-19 14:41:26 +0100

    Ukończenie egzaminu

--- rezerwacja.php
+++ rezerwacja.php
@@ -1,22 +1,26 @@
 <?php
-    if (isset($_POST['submit'])) {
-        $data = $_POST['data'];
-        $ilosc_osob = $_POST['ilosc_osob'];
-        $nr_tel = $_POST['tel_number'];
-        $zgoda = $_POST['dane_osobowe'];
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "baza2";
+    
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    $data = $_POST['data'];
+    $ilosc_osob = $_POST['ilosc_osob'];
+    $nr_tel = $_POST['tel_number'];
         
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "baza2";
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
-          }
+                }
+        
+        if (isset($_POST['submit'])) {
 
-          $sql = "INSERT INTO rezerwacje VALUES (NULL, 1, "2017-07-04", 4, "111222333" )";
-        echo "Dodano rezerwację do bazy";
+          $sql = "INSERT INTO rezerwacje VALUES (NULL, NULL, '$data', $ilosc_osob, $nr_tel )";
+          $dodawanie_danych = mysqli_query($conn, $sql);
+        
+          echo "Dodano rezerwację do bazy";
 
         mysqli_close($conn);
-    };
+            };
 ?>
